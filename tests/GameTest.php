99aa2b2dceecb8abc5e57c88de2d43670c639c7d
Florin Buga <florin.buga@gmail.com>
2016-01-23 14:29:47 +0000

    Write testGameStartsWithEmptyGrid()

--- tests/GameTest.php
+++ tests/GameTest.php
@@ -1,8 +1,25 @@
 <?php namespace Florin\TicTacToe;
 
+/**
+ * Tests the Tic-tac-toe game rules:
+ *
+ * Tic-tac-toe is played by two players who take turns marking the spaces in a 3×3 grid
+ * using X (for the 1st player) and O (for the 2nd)
+ * The player who succeeds in placing three of their marks in a horizontal, vertical, or diagonal row wins the game.
+ */
 class GameTest extends \PHPUnit_Framework_TestCase
 {
-    public function test()
+    /**
+     * @covers \Florin\TicTacToe\Game::__construct
+     */
+    public function testGameStartsWithEmptyGrid()
     {
+        $game = new Game();
+        $emptyGrid = [
+            [null, null, null],
+            [null, null, null],
+            [null, null, null]
+        ];
+        $this->assertEquals($game->showGrid(), $emptyGrid);
     }
 }
