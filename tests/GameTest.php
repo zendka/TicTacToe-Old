58edecd30af9d602b54fe11da1b3579aeacb70e9
Florin Buga <florin.buga@gmail.com>
2016-01-23 20:32:50 +0000

    Write testPlayersMarkIsRegisteredCorrectly()

--- tests/GameTest.php
+++ tests/GameTest.php
@@ -6,11 +6,14 @@
  * Tic-tac-toe is played by two players who take turns marking the spaces in a 3×3 grid
  * using X (for the 1st player) and O (for the 2nd)
  * The player who succeeds in placing three of their marks in a horizontal, vertical, or diagonal row wins the game.
+ *
+ * Assumption: the 1st player (using X) is always the human
  */
 class GameTest extends \PHPUnit_Framework_TestCase
 {
     /**
      * @covers \Florin\TicTacToe\Game::__construct
+     * @covers \Florin\TicTacToe\Game::showGrid
      */
     public function testGameStartsWithEmptyGrid()
     {
@@ -20,6 +23,21 @@ class GameTest extends \PHPUnit_Framework_TestCase
             [null, null, null],
             [null, null, null]
         ];
-        $this->assertEquals($game->showGrid(), $emptyGrid);
+        $this->assertEquals($emptyGrid, $game->showGrid());
+    }
+
+    public function testPlayersMarkIsRegisteredCorrectly()
+    {
+        $game = new Game();
+
+        $position = ['row' => 0, 'col' => 0];
+        $game->playerMarks($position);
+
+        $expectedGrid = [
+            ['X', null, null],
+            [null, null, null],
+            [null, null, null]
+        ];
+        $this->assertEquals($expectedGrid, $game->showGrid());
     }
 }
