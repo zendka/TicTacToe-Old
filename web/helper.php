9b27da58d45f09abd5dcc6822ce52a857d024bce
Florin Buga <florin.buga@gmail.com>
2016-01-25 03:32:57 +0000

    Refactor helper.php

--- web/helper.php
+++ web/helper.php
@@ -1,6 +1,21 @@
 <?php
+/*
+ * This file handles the HTTP requests:
+ * - processes input
+ * - plays the game round
+ * - initialises the variables to be used in the template file
+ */
+
 require_once dirname(__DIR__) . '/src/autoload.php';
 
+use Florin\TicTacToe\Game as Game;
+
+/**
+ * Cleans up input grid
+ *
+ * @param  array $inputGrid The input grid
+ * @return array $grid      The cleaned grid
+ */
 function getGridFromInput($inputGrid)
 {
     for ($i=0; $i<3; $i++) {
@@ -16,30 +31,20 @@ function getGridFromInput($inputGrid)
     return $grid;
 }
 
-$gameOver = false;
+// Process input
+$firstPlayer = isset($_GET['first']) ? $_GET['first'] : null;
+$cleanedGrid = isset($_GET['grid']) ? getGridFromInput($_GET['grid']) : null;
 
-if (isset($_GET['grid'])) {
-    // Continue game
-    $currentGrid = getGridFromInput($_GET['grid']);
-    $game = new Florin\TicTacToe\Game($currentGrid);
+// Play round
+$game = new Game($cleanedGrid);
+$newGame = is_null($cleanedGrid) ? true : false;
+if (!$newGame || ($newGame && $firstPlayer == 'computer')) {
     $game->computerMarks();
-
-    if ($game->isOver()) {
-        $gameOver = true;
-
-        if ($game->computerWon()) {
-            $message = "Game over. Computer Won.";
-        } else {
-            $message = "Game over. It's a draw.";
-        }
-    }
-} else {
-    // Start new game
-    $game = new Florin\TicTacToe\Game();
-
-    if (isset($_GET['first']) && $_GET['first'] == 'computer') {
-        $game->computerMarks();
-    }
 }
 
+// Initialise variables to unse in template
 $grid = $game->getGrid();
+$gameOver = $game->isOver();
+if ($gameOver) {
+    $message = $game->computerWon() ? "Game over. Computer Won." : "Game over. It's a draw.";
+}
